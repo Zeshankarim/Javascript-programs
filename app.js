a4c88dc8ca96be6aabfdc334a3bfc9d5a410c83e
zeshan karim <zeshankarim64@gmail.com>
2020-06-14 14:49:12 +0500

    ten complete

--- app.js
+++ app.js
@@ -271,4 +271,111 @@ var totalper = totalObtMarkss/totalMarkks*100;
 document.write("<br/>"+"total "+totalMarkks+" "+totalObtMarkss +" " +totalper);
 
 
+// Chapter 9-11
+// Q1
+var cityname=prompt("Enter a City name");
+if (cityname=== "karachi"){
+document.write("Welcome to city of light");
+}
+// Q2
+var gender=prompt("Enter your gender");
+if(gender==="Male"){
+    document.write("Good Moring Sir");
+}
+if(gender==="Female"){
+document.write("Good morning Ma'am ");
+}
+// Q3
+var color=prompt("Enter the colour of traffic road sigal");
+if(color==="red"){
+    document.write("Must stop");
+}
+else if(color==="yellow"){
+    document.write("Ready to move");
+}
+else if(color==="green"){
+    document.write("Move now");
+}
+else{
+    document.write("Enter correct color");
+}
 
+
+// Q4
+var fuel=+prompt("Enter your remaining fuel");
+if(fuel===0.25){
+    document.write("Please refill the fuel in your car");
+}
+
+// Q5
+var a = 4;
+if (++a === 5) {
+    alert("given condition for variable a is true");
+}
+var b = 82;
+if (b++ === 83) {
+    alert("given condition for variable b is true");
+}
+var c = 12;
+if (c++ === 13) {
+    alert("condition 1 is true");
+}
+if (c === 13) {
+    alert("condition 2 is true");
+}
+if (++c < 14) {
+    alert("condition 3 is true");
+}
+if (c === 14) {
+    alert("condition 4 is true");
+    var materialCost = 20000;
+    var laborCost = 2000;
+    var totalCost = materialCost + laborCost;
+    if (totalCost === laborCost + materialCost) {
+        alert("The cost equals");
+    }
+    if (true) {
+        alert("True");
+    }
+    if (false) {
+        alert("False");
+    }
+    var b = 82;
+    if (b++ === 83) {
+        alert("given condition for variable b is true");
+    } \
+    var c = 12;
+    if (c++ === 13) {
+        alert("condition 1 is true");
+    }
+    if (c === 13) {
+        alert("condition 2 is true");
+    }
+    if (++c < 14) {
+
+        alert("condition 3 is true");
+    }
+    if (c === 14) {
+        alert("condition 4 is true");
+    }
+    var materialCost = 20000;
+    var laborCost = 2000;
+    var totalCost = materialCost + laborCost;
+    if (totalCost === laborCost + materialCost) {
+        alert("The cost equals");
+    }
+    if (true) {
+        alert("True");
+    }
+    if (false) {
+        alert("False");
+    }  
+
+
+
+
+
+// var chemistry=+prompt("Enter your chemistry marks");
+// var math=+prompt("Enter your Math marks");
+// var english=+prompt("Enter your Eglish marks");
+// if(){}
